a80eb149775a167188bd2d7f78b3ccf9ec243a5b
soedar <soedar.sg@gmail.com>
2012-07-05 14:35:19 +0800

    Initial Commit

---
+++ proj1-cake/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  proj1-cake
+//
+//  Created by Soedar on 5/7/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ proj1-cake/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  proj1-cake
+//
+//  Created by Soedar on 5/7/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
